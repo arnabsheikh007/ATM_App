7b1a76fe0ad04b69a4246d8c54bb99c2e65e47c3
arnab <arnab@ARNAB>
2022-11-08 15:12:39 +0600

    Add project files.

---
+++ ATM_App/App/ATM_App.cs
@@ -0,0 +1,13 @@
+﻿using ATM_App.UI;
+using System;
+
+namespace ATM_App
+{
+    class ATM_App
+    {
+        static void Main(string[] args)
+        {
+            AppScreen.Welcome()
+        }
+    }
+}
---
+++ ATM_App/UI/AppScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_App.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            Console.Clear();
+            Console.Title = "My ATM App";
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\n\n-----------Welcome to my ATM App----------\n\n");
+            Console.WriteLine("Please Enter your ATM Card");
+            Console.WriteLine("Actual ATM Machine will read the number validate your card and accept it");
+            PressEnterToContinue();
+        }
+
+        private static void PressEnterToContinue()
+        {
+            Console.WriteLine("\nPress Enter to continue...\n");
+            Console.ReadLine();
+        }
+    }
+}
